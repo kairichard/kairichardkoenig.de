79495763ff5455426118e81462f5c395521c3071
Kai Richard Koenig <kai.richard.koenig@gmail.com>
2019-10-09 12:49:05 +0200

    Fix extracting

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 yum install -y wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.58.3/hugo_extended_0.58.3_Linux-64bit.tar.gz
-tar -xzf hugo_0.58.3_Linux-64bit.tar.gz
+tar -xzf hugo_extended_0.58.3_Linux-64bit.tar.gz
 
 git clone https://github.com/jugglerx/hugo-whisper-theme.git themes/hugo-whisper-theme
 
