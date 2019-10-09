5242969e6f3a120202b9207586c09dc5542e7e22
Kai Richard Koenig <kai.richard.koenig@gmail.com>
2019-10-09 12:43:34 +0200

    Turn theme into submodule

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@ yum install -y wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.58.3/hugo_0.58.3_Linux-64bit.tar.gz
 tar -xzf hugo_0.58.3_Linux-64bit.tar.gz
 
+git submodule update --recursive --remote
+
 ./hugo
