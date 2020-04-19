2c7ff445ba27e801629fae11b713dc2096401e2e
Kai Richard Koenig <kai.richard.koenig@gmail.com>
2020-04-19 12:58:39 +0200

    Now does not use the git to checkout the source

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ yum install -y wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.58.3/hugo_extended_0.58.3_Linux-64bit.tar.gz
 tar -xzf hugo_extended_0.58.3_Linux-64bit.tar.gz
 
-./hugo
+./hugo --gc --minify --i18n-warnings --log
